d99b07f3f893f122ab349c318af9e29e7404f0f0
takuma39 <takuma.sato.job@gmail.com>
2022-07-19 17:22:50 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">header1</x-slot>
     test1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">header2</x-slot>
     test2
 </x-tests.app>
