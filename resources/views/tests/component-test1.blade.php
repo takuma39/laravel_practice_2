9b68890481bd6be9f93e69dbb57f7947618fe669
takuma39 <takuma.sato.job@gmail.com>
2022-07-19 18:23:33 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,5 @@
 <x-tests.app>
     <x-slot name="header">header1</x-slot>
     test1
-    <x-tests.card title="タイトル" content="本文" />
+    <x-tests.card title="タイトル" content="本文" :message="$message" />
 </x-tests.app>
