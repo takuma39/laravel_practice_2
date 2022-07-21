f5b19f895dfbede7051b18ed1d6ffd5ee39a589e
takuma39 <takuma.sato.job@gmail.com>
2022-07-21 10:13:42 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+'title' => 'title初期値',
+'content' => 'content初期値',
+'message' => 'message初期値',
+])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{ $title}}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,6 @@
     <x-slot name="header">header1</x-slot>
     test1
     <x-tests.card title="タイトル" content="本文" :message="$message" />
+
+    <x-tests.card />
 </x-tests.app>
