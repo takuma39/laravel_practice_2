b1b3bf55100db66d04ebe70737c460046caed6d2
takuma39 <takuma.sato.job@gmail.com>
2022-07-21 15:11:34 +0900

    サービス・プロバイダーテスト

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
-
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 
 Route::get('/servicecontainertest', [LifeCycleController::class, 'showServiceContainerTest']);
+Route::get('/serviceprovidertest', [LifeCycleController::class, 'showServiceProviderTest']);
 
 require __DIR__ . '/auth.php';
