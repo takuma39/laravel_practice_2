c2e56bd7e0e719a22891ff582c1bcf29c6892319
takuma39 <takuma.sato.job@gmail.com>
2022-07-27 11:41:51 +0900

    middleware, abort(404)

--- routes/owner.php
+++ routes/owner.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Owner\ShopController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,6 +23,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('owner.welcome');
+});
+
+Route::prefix('shops')->middleware('auth:owners')->group(function () {
+    Route::get('index', [ShopController::class, 'index'])->name('shops.index');
+    Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
+    Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
 
 Route::get('/dashboard', function () {
