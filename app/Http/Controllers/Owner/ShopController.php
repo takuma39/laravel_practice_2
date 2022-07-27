28658abbbd69a4757e3254ddf1294326d7d91f4c
takuma39 <takuma.sato.job@gmail.com>
2022-07-27 14:04:47 +0900

    リサイズなしアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -38,11 +39,19 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd('test');
+        // dd('test');
+        $shop = Shop::findOrFail($id);
+        return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update($id)
+    public function update(Request $request, $id)
     {
-        dd('test');
+        // dd('test');
+        $imageFile = $request->image; //一時保存
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            Storage::putFile('public/shops', $imageFile);
+        }
+
+        return redirect()->route('owner.shops.index');
     }
 }
