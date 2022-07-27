99e07b0ec47a8bc65f2bd1cfff9e4991675e7cf9
takuma39 <takuma.sato.job@gmail.com>
2022-07-27 18:03:03 +0900

    shop edit,updae

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -49,13 +49,32 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, $id)
     {
-        // dd('test');
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'information' => 'required|string|max:1000',
+            'is_selling' => 'required',
+        ]);
+
         $imageFile = $request->image; //一時保存
         if (!is_null($imageFile) && $imageFile->isValid()) {
 
             $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
-        return redirect()->route('owner.shops.index');
+        $shop = Shop::findOrFail($id);
+        $shop->name =  $request->name;
+        $shop->information =  $request->information;
+        $shop->is_selling =  $request->is_selling;
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            $shop->fileName = $fileNameToStore;
+        }
+        $shop->save();
+
+        return redirect()
+            ->route('owner.shops.index')
+            ->with([
+                'message' => '店舗情報を修正しました。',
+                'status' => 'info'
+            ]);
     }
 }
---
+++ resources/views/components/shop-thumbnail.blade.php
@@ -0,0 +1,7 @@
+<div class="">
+    @if (empty($filename))
+        <img src="{{ asset('images/no_image.jpg') }}">
+    @else
+        <img src="{{ asset('storage/shops/' . $filename) }}">
+    @endif
+</div>
