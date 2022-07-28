bb0cab85fa058c0d8667a67a5815b4e4d2391f30
takuma39 <takuma.sato.job@gmail.com>
2022-07-28 17:39:41 +0900

    image delete

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -7,6 +7,6 @@ if (session('status') === 'alert') {
     $bgColor = 'bg-red-500';
 } @endphp
 @if (session('message'))
-    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white mb-4">
         {{ session('message') }} </div>
 @endif
