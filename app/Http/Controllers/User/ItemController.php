0a90562e19a9666f74788989c044140e4f11e300
takuma39 <takuma.sato.job@gmail.com>
2022-08-04 11:26:46 +0900

    user show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\DB;
 class ItemController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -44,5 +49,12 @@ class ItemController extends Controller
             ->get();
 
         return view('user.index', compact('products'));
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
 }
