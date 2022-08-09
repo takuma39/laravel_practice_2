06cf04a38a7bfd112532a6fa219959f36d16b706
takuma39 <takuma.sato.job@gmail.com>
2022-08-09 10:47:59 +0900

    オーナー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
+use App\Jobs\SendOrderedMail;
 
 class CartController extends Controller
 {
@@ -62,15 +63,7 @@ class CartController extends Controller
 
     public function checkout()
     {
-
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
         $user = User::findOrFail(Auth::id());
-
-        //メール送信
-        SendThanksMail::dispatch($products, $user);
-        dd('ユーザーメール送信テスト');
-
         $products = $user->products;
 
         $lineItems = [];
@@ -129,6 +122,16 @@ class CartController extends Controller
 
     public function success()
     {
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+        //メール送信
+        SendThanksMail::dispatch($products, $user);
+        foreach ($products as $product) {
+            SendOrderedMail::dispatch($product, $user);
+        }
+        // dd('ユーザーメール送信テスト');
+
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
